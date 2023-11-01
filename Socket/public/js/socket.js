516a04df58482031f0d157685db0ee8b0fb6893f
arunjegan <arun96.nova@gmail.com>
2023-11-01 13:30:56 +0530

    Socket with room

--- Socket/public/js/socket.js
+++ Socket/public/js/socket.js
@@ -1,15 +1,28 @@
 let socket = io();
-socket.on('connect', function () {
-    console.log('Server has been connnected')
-});
-socket.emit('createMessage',{
-     'from' : 'user1',
-     'Text' : 'Im Fine',
-     "Date" : new Date().getTime()
+ let myRoom = prompt('Enter Your Room');
+ console.log('Your Room is',myRoom);
+document.addEventListener('submit',(e) => {
+    e.preventDefault();
+    console.log(e.target.type)
+    if(e.target.type != ''){
+        let newMessage= document.querySelector('input[type="text"]').value;
+        socket.on('connect', function () {
+            console.log('Server has been connnected');
+            socket.emit('room',myRoom);
+        });
+        socket.emit('createMessage',{
+             'from' : 'user1',
+             'Text' : 'Im Fine',
+             "Date" : new Date().getTime()
+        })
+        socket.on('newMessage', (msg) => {
+            console.log('newMessage',msg);
+        })
+        socket.on('disconnect', function ()  {
+            console.log('Server has been disconnected');
+        });
+    }
+    
 })
-socket.on('newMessage', (msg) => {
-    console.log('newMessage',msg);
-})
-socket.on('disconnect', function ()  {
-    console.log('Server has been disconnected');
-})
+    
+
--- Socket/server.js
+++ Socket/server.js
@@ -4,24 +4,29 @@ const http = require('http');
 const socket = require('socket.io');
 const path = require('path');
 const port = 8000;
-
+const generateMessage = require('./utilits/messages.js')
 const server = http.createServer(app);
 const io = socket(server);
+let myRoom = '';
+app.use(express.static(path.join(__dirname,'public')));
+app.get('/chat',(req,res) => {
+    myRoom = req.query.room;
+    res.sendFile(path.join(__dirname,'/public/index.html'));
+})
 io.on('connection', (socketEvent) => {
-    console.log('New user connection');
-    
+    console.log('New user connection',myRoom);
     socketEvent.on('createMessage' ,(msg) => {
-        console.log('createMessage listen',msg)
-        socketEvent.emit('newMessage',{
+        socketEvent.join(myRoom);      
+        io.to(myRoom).emit('newMessage',{
             'From' : 'Admin',
             'Text' : 'Welcome to Chat APP',
             "Date" : new Date().getTime()
         });
-        socketEvent.broadcast.emit('newMessage',{
-            'From' : 'Admin',
-            'Text' : 'A new user Joined',
-            "Date" : new Date().getTime()
-        });
+        // socketEvent.broadcast.emit('newMessage',{
+        //     'From' : 'Admin',
+        //     'Text' : 'A new user Joined',
+        //     "Date" : new Date().getTime()
+        // });
     });
     socketEvent.on('disconnect', () => {
         console.log('New user has been disconnect');
@@ -29,10 +34,7 @@ io.on('connection', (socketEvent) => {
 });
 
 
-app.use(express.static(path.join(__dirname,'public')));
-app.get('/',(req,res) => {
-    res.sendFile(path.join(__dirname,'/public/index.html'));
-})
+
 server.listen(port,() => {
     console.log(`App is listening on ${port}`);
 })
